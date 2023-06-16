b43dfb110ac9aaf02f62ab97e019917a7bfc7e4c
pthwong <tszhim.wong1010@yahoo.com>
2023-06-16 15:47:41 +0800

    Adjust the code

--- routes/pubLogin.js
+++ routes/pubLogin.js
@@ -15,27 +15,30 @@ const SECRET_KEY = generateSecretKey();
 console.log("SECRET_KEY:\n", SECRET_KEY);
 
 router.post("/", (req, res) => {
-    const { pubEmail, pubPassword } = req.body;
-  
-    if (!pubEmail || !pubPassword) {
-      return res.status(400).send('Missing username or password');
-    }
-  
-    const hashedpubPassword = crypto.createHash('sha1').update(pubPassword).digest('hex');
-    
-    const sql = 'SELECT * FROM Public WHERE pubEmail = ? AND pubPassword = ?';
-    db.query(sql, [pubEmail, hashedpubPassword], (err, result) => {
-      if (err) throw err;
-      
-      if (result.length > 0) {
-        const public = result[0];
-        const token = jwt.sign({ pubEmail: public.pubEmail }, SECRET_KEY); // replace with your secret key
-        res.json({ token });
-      } else {
-        res.status(401).send('Invalid username or password');
-      }
-    });
-  });
+  const { pubEmail, pubPassword } = req.body;
 
-  // Export the server
+  if (!pubEmail || !pubPassword) {
+    return res.status(400).send("Missing email or password");
+  }
+
+  const hashedPubPassword = crypto
+    .createHash("sha1")
+    .update(pubPassword)
+    .digest("hex");
+
+  const sql = "SELECT * FROM Public WHERE pubEmail = ? AND pubPassword = ?";
+  db.query(sql, [pubEmail, hashedPubPassword], (err, result) => {
+    if (err) throw err;
+
+    if (result.length > 0) {
+      const public = result[0];
+      const token = jwt.sign({ pubEmail: public.pubEmail }, SECRET_KEY); // replace with your secret key
+      res.json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid email or password" });
+    }
+  });
+});
+
+// Export the server
 module.exports = router;
