3ba4bb0d2e48e6e8e6bfeebe2ab3f14cf77ea45a
pthwong <tszhim.wong1010@yahoo.com>
2023-06-18 16:39:57 +0800

    Add favourite and delete favourite cat functions created

---
+++ routes/addFavourite.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the database connection object
+const db = require("../dbConnect");
+
+router.post("/", (req, res) => {
+  const { pubEmail, catID } = req.body;
+
+  const sql = "INSERT INTO CatFavourite (pubEmail, catID) VALUES (?, ?)";
+  
+  db.query(sql, [pubEmail, catID], (err, result) => {
+    if (err) {
+      // Return a 500 status code and error message
+      return res.status(500).json({ error: err.message });
+    }
+
+    // Successfully added to favourites
+    res.status(200).json({ message: 'Cat added to favourites successfully' });
+  });
+});
+
+// Export the router
+module.exports = router;
---
+++ routes/delFavourite.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the database connection object
+const db = require("../dbConnect");
+
+router.delete("/", (req, res) => {
+  const { pubEmail, catID } = req.body;
+
+  // First, check if the entry exists
+  const checkSql =
+    "SELECT * FROM CatFavourite WHERE pubEmail = ? AND catID = ?";
+
+  db.query(checkSql, [pubEmail, catID], (err, result) => {
+    if (err) {
+      // Return a 500 status code and error message
+      return res.status(500).json({ error: err.message });
+    }
+
+    // If the entry does not exist, return 404
+    if (result.length === 0) {
+      return res.status(404).json({ error: "Cat not found in favourites" });
+    }
+
+    // If the entry exists, attempt to delete it
+    const deleteSql =
+      "DELETE FROM CatFavourite WHERE pubEmail = ? AND catID = ?";
+
+    db.query(deleteSql, [pubEmail, catID], (err, result) => {
+      if (err) {
+        // Return a 500 status code and error message
+        return res.status(500).json({ error: err.message });
+      }
+
+      // Successfully removed from favourites
+      res
+        .status(200)
+        .json({ message: "Cat removed from favourites successfully" });
+    });
+  });
+});
+
+// Export the router
+module.exports = router;
